e6cf2a61aea29ee6d45f685db28fe5d165183d95
arahmanarif <rjarif0958@gmail.com>
2023-07-21 18:09:48 +0600

    task added auth middleware

--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('tasks', TaskController::class);
+    Route::get('task/inactive/{id}', [TaskController::class, 'statusInactive'])->name('task.inactive');
+    Route::get('task/active/{id}', [TaskController::class, 'statusActive'])->name('task.active');
 });
 
-Route::resource('tasks', TaskController::class);
-Route::get('task/inactive/{id}', [TaskController::class, 'statusInactive'])->name('task.inactive');
-Route::get('task/active/{id}', [TaskController::class, 'statusActive'])->name('task.active');
 
 require __DIR__.'/auth.php';
